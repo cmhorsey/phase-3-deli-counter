d8f84608cc3aefc10a1c41ff5fe4048d0d2170bf
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-12 14:37:33 -0400

    Complete lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    line_description = "The line is currently:"
+    array.each.with_index(1) do |name, index|
+      line_description += " #{index}. #{name}"
+    end
+    puts line_description
+  end
+end
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+  array.push(name)
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = array.shift
+    puts "Currently serving #{current_customer}."
+  end
+end
